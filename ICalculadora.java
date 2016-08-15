5389d3ec09baa94ce98ac6940ead9db1bf5db22a
Gladys20 <dela15755@uvg.edu.gt>
2016-08-15 12:31:51 -0600

    Create ICalculadora.java

---
+++ ICalculadora.java
@@ -0,0 +1,15 @@
+/**
+ * @author 
+ * Clase I_Calculadora
+ * Interfaz de la calculadora
+ * @version 24/07/2016
+ */
+
+public interface ICalculadora {
+
+	//Metodos de la interfaz
+	public int Calcular(String vector);
+	public String toString();
+	public String LeerArchivo (String direccion);
+}
+ 
