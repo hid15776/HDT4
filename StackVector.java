a5cb2b9ee5a866915a60077b0bf4a8dfa97a3cab
hid15776 <hid15776@uvg.edu.gt>
2016-08-15 16:14:37 -0600

    Update StackVector.java

--- StackVector.java
+++ StackVector.java
@@ -16,13 +16,13 @@ public class StackVector<E> extends AbstractStack<E>{
         	data = new Vector<E>();
     	}
     
-    	public void push(E element) {
+    	public void Push(E element) {
         // pre: no se necesita
         // post: un nuevo dato se guarda en la pila
         	data.add(element);
     	}
 
-	public E pop() {    
+	public E Pop() {    
         // pre: se realiza solamente si la pila tiene algun dato guardado
         // post: se saca de la pila el Ãºltimo dato guardado
         	E element = data.lastElement();
@@ -30,20 +30,20 @@ public class StackVector<E> extends AbstractStack<E>{
         	return element;
     	}
     
-    	public E get() {
+    	public E Get() {
         // pre: se realiza solamente si la pila tiene algun dato guardado
         // post: indica cual ue el ultimo dato guardado
         	return data.get(size() - 1);
     	}
 
     
-    	public boolean isEmpty() {
+    	public boolean IsEmpty() {
         // pre: no se necesita
         // post: devuelve true si la pila esta vacia y false si tiene algun dato guardado
         	return size() == 0;
     	}
 
-    	public int size() {
+    	public int Size() {
         // pre: no se necesita
         // post: cuenta la cantidad de datos en la pila
         return data.size();
