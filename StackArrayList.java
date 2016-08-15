59cf99f55ca71529de103557e991d5d5872fe25e
hid15776 <hid15776@uvg.edu.gt>
2016-08-15 16:16:52 -0600

    Update StackArrayList.java

--- StackArrayList.java
+++ StackArrayList.java
@@ -14,30 +14,30 @@ public class StackArrayList<E> extends AbstractStack<E>{
 		data = new ArrayList<E>();
 	}
 
-	public void push(E element){
+	public void Push(E element){
 	// post: the value is added to the stack
 	//          will be popped next if no intervening push
 		data.add(element);
 	}
 
-	public E pop(){
+	public E Pop(){
 	// pre: stack is not empty
 	// post: most recently pushed item is removed and returned
 		return data.remove(size()-1);
 	}
 
-	public E get(){
+	public E Get(){
 	// pre: stack is not empty
 	// post: top value (next to be popped) is returned
 		return data.get(size() - 1);
 	}
 	
-	public int size(){
+	public int Size(){
 	// post: returns the number of elements in the stack
 		return data.size();
 	}
   
-	public boolean isEmpty(){
+	public boolean IsEmpty(){
 	// post: returns true if and only if the stack is empty
 		return size() == 0;
 	}
