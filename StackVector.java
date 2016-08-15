c7cfc9dd19722c207e5bf74e31d2b9301ba0876d
hid15776 <hid15776@uvg.edu.gt>
2016-08-15 16:10:15 -0600

    Update StackVector.java

--- StackVector.java
+++ StackVector.java
@@ -7,50 +7,45 @@
 import java.util.Vector;
 
 public class StackVector<E> extends AbstractStack<E>{
-    protected   Vector<E> data;
-    protected   int indice   = 0;
+	protected   Vector<E> data;
+    	protected   int indice   = 0;
     
-    public StackVector()
-	 // pre: no se necesita
+    	public StackVector(){
+ 	// pre: no se necesita
 	// post: constructor de una pila
-    {
-            data = new Vector<E>();
-    }
+        	data = new Vector<E>();
+    	}
     
-    public void push(E item) {
+    	public void push(E element) {
         // pre: no se necesita
         // post: un nuevo dato se guarda en la pila
-        data.add(item);
-    }
+        	data.add(element);
+    	}
 
-
-    public E pop() {    
+	public E pop() {    
         // pre: se realiza solamente si la pila tiene algun dato guardado
-         // post: se saca de la pila el Ãºltimo dato guardado
-        E item = data.lastElement();
-        data.remove(data.size()-1);
-        return item;
-
-    }
-
+        // post: se saca de la pila el Ãºltimo dato guardado
+        	E element = data.lastElement();
+        	data.remove(data.size()-1);
+        	return element;
+    	}
     
-    public E peek() {
+    	public E get() {
         // pre: se realiza solamente si la pila tiene algun dato guardado
         // post: indica cual ue el ultimo dato guardado
-        return data.get(size() - 1);
-    }
+        	return data.get(size() - 1);
+    	}
 
     
-    public boolean empty() {
+    	public boolean isEmpty() {
         // pre: no se necesita
         // post: devuelve true si la pila esta vacia y false si tiene algun dato guardado
-        return size() == 0;
-    }
+        	return size() == 0;
+    	}
 
-    public int size() {
+    	public int size() {
         // pre: no se necesita
         // post: cuenta la cantidad de datos en la pila
         return data.size();
-    }
-    
+    	}
 }
