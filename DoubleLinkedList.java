564d7778a39b025eef42941dda332aaf6b977ca7
hid15776 <hid15776@uvg.edu.gt>
2016-08-15 19:19:16 -0600

    Update DoubleLinkedList.java

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -30,7 +30,7 @@ public class DoubleLinkedList<E> extends AbstractList<E> {
 	}
 
 	public void addFirst(E value){
-		head = new DoubleLinkedList<E>(value, head, null);
+		head = new DoubleNode<E>(value, head, null);
 		if (tail == null) tail = head;
 		count++;
 	}
@@ -57,6 +57,132 @@ public class DoubleLinkedList<E> extends AbstractList<E> {
 	
 	public void setCount(int count) {
 		this.count = count;
+	}
+
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addLast(E value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public E getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public E getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public E removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public E removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public E remove(E value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void add(E value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public E remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public E get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int indexOf(E value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int lastIndexOf(E value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public E get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public E set(int i, E o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void add(int i, E o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public E remove(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 
