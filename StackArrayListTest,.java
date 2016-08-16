a46df5fb1e9df663ffee54a34bf8c4b43a625ca4
Gladys20 <dela15755@uvg.edu.gt>
2016-08-15 19:49:34 -0600

    Create StackArrayListTest,.java

---
+++ StackArrayListTest,.java
@@ -0,0 +1,48 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+/**
+ * @author Gladys De La Roca, Jackeline Hidalgo
+ * Clase PilaTest
+ * Prueba JUnit 
+ * @version 24/07/2016
+ */
+public class StackArrayListTest extends TestCase {
+	
+	@Test
+	public void TestIsEmpty() {
+		StackArrayList s = new StackArrayList();
+		s.IsEmpty();
+		boolean parametro = false;
+		boolean paramreal = s.IsEmpty();
+		assertEquals(parametro, paramreal);
+		s.IsEmpty();
+		boolean parametro1 = true;
+		boolean paramreal1 = s.IsEmpty();
+		assertEquals(parametro1, paramreal1);
+	}
+	
+	@Test
+    public void TestPush() {
+		StackArrayList s = new StackArrayList();
+        String parametro = "";
+        s.Push(parametro);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("Fallo.");
+    }
+
+    /**
+     * Test of pop method, of class iPila.
+     */
+    @Test
+    public void TestPop() {
+    	StackArrayList s = new StackArrayList();
+        String parametro = "";
+        Object paramreal = s.Pop();
+        assertEquals(parametro, paramreal);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("Fallo");
+    }
+
+
+
+}
